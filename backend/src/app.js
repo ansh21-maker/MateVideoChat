4143333756d574a331dcc1063ee95f302a6f455e
Anshul <anshulkuama819@gmail.com>
2026-03-14 22:58:40 +0530

    Authenication routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
 
 
 import cors from "cors";
+import userRoutes from "./routes/users.route.js";
 import { connect } from "node:http2";
 
 
@@ -17,6 +18,8 @@ app.set("port",(process.env.PORT||8000));
 app.use(cors());
 app.use(express.json({limit:"40kb"}));
 app.use(express.urlencoded({limit:"40kb",extended:true}));
+app.use("/api/v1/users",userRoutes);
+
 
 
 
---
+++ backend/src/controllers/user.Controller.js
@@ -0,0 +1,55 @@
+import bcrypt,{hash} from "bcrypt";
+
+const login=async(req,res)=>{
+const {username,password}=req.body;
+if(!username||password){
+    return res.status(400).json({message:"Please provide"})
+}
+
+try{
+    const user=await User.find({username});
+    if(!user){
+        return res.status(httpStatus.NOT_FOUND).json({message:"user not found"});
+    }
+
+    if(bcrypt.compare(password,user.password)){
+        let token =crypto.randomBytes(20).toString("hex");
+        user.token=token;
+        await user.save();
+        return res.status(httpStatus.OK).json({token:token});
+    }
+}catch(e){
+return res.status(500).json({message:`Somthing went wrong ${e}`});
+}
+}
+
+
+export {login, register}; 
+
+
+
+
+
+const register=async(req,res)=>{
+const {name, username, password}=req.body;
+
+
+try{
+    const  existinUser=await User.findOne({username});
+    if(existinUser){
+        return res.status(httpStatus.FOUND).json({message:"user already exist"});
+    }
+    const hashPassword=await bcrypt.hash(password,10);
+    const newUser=new User({
+name:name,
+username,username,
+hash:hashedPassword,
+    });
+    await newUser.save();
+    res.status(httpStatus.CREATED).json({message:"user registered"})
+}
+
+catch(e){
+res.json({message:`something went wrong ${e}`});
+}
+}
